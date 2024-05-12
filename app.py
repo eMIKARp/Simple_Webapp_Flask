6b24376248a8ce4abeae1381c76cb00f9c1ba162
eMIKARp <emil.karpowicz@gmail.com>
2024-05-12 15:22:15 +0200

    App file created

---
+++ app.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask
+
+app = Flas(__name__)
+
+@app.route("/")
+def main():
+    return "Welcome!"
+
+
+@app.route("/how are you")
+def hello():
+    return "I am good, how about you?"
+
+
+if __name__() == "__main__":
+    app.run()
